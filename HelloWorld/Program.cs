da45f58c367b15d648349a5237cddc583863170f
Jose Sison <68295496+josesison1@users.noreply.github.com>
2020-08-22 20:21:29 -0700

    Add files via upload

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        // My very first C# Hello World application using Visual Studio
+            Console.WriteLine("Hello Jose, welcome to your first day!");
+        }
+    }
